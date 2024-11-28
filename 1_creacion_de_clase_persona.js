4eb70bac8eee802276e85e2d839df5a98198e125
gonzalo341 <136021526+gonzalo341@users.noreply.github.com>
2024-11-28 12:56:10 -0300

    Add files via upload

---
+++ 1_creacion_de_clase_persona.js
@@ -0,0 +1,11 @@
+//creacion de clase persona
+class Persona {
+    constructor(nombre,edad,altura,colorDePelo){
+        this.nombre = nombre
+        this.edad = edad
+        this.altura = altura
+        this.colorDePelo = colorDePelo
+    }
+}
+
+let persona1 = new Persona('Gonzalo', 15, 1.72, "negro")
